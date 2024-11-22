60ee22ff09380b3d2e3b1dedbdcd5ad3b48e8613
Stam2000 <sopemmanuel@gmail.com>
2024-11-22 23:17:36 +0100

    c
    c

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-export const sql = neon("postgresql://finance_owner:V5EHXAkLs6au@ep-hidden-poetry-a59ri0wt.us-east-2.aws.neon.tech/finance?sslmode=require");
+export const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sql);
 
 
