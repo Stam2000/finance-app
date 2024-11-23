3b0901c7ff497989cfb8341901eca52a920e8e84
Stam2000 <sopemmanuel@gmail.com>
2024-11-24 00:15:18 +0100

    Revert "c"
    This reverts commit c70988b7c632902b9de5e533074e8f97535bbed7.

--- components/welcoe-msg.tsx
+++ components/welcoe-msg.tsx
@@ -1,24 +1,18 @@
 "use client"
 import QuestionChat from "./AiQuestion";
 import { useUpdateChat } from "@/features/chat/hook/useUpdateMessage";
-import { useEffect } from "react";
 
 
 
 export const WelcomeMsg =()=>{
     const {personaInfo} = useUpdateChat()
-    let i = {name:""}
-    if(personaInfo){
-        i = JSON.parse(personaInfo)
-    }
-    console.log(personaInfo)
-
+    const i = JSON.parse(personaInfo)
 
 
     return(
         <div className="space-y-4 mb-4" >
             <h1 className="text-2xl lg:text-4xl text-white font-medium" >
-                Welcome Back, {personaInfo && i.name}
+                Welcome Back, {i.name}
             </h1>
             <QuestionChat />
             <p className="text-sm text-[#dfecff] lg:text-base" >
