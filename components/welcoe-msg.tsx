c70988b7c632902b9de5e533074e8f97535bbed7
Stam2000 <sopemmanuel@gmail.com>
2024-11-23 12:06:45 +0100

    c
    c

--- components/welcoe-msg.tsx
+++ components/welcoe-msg.tsx
@@ -1,18 +1,24 @@
 "use client"
 import QuestionChat from "./AiQuestion";
 import { useUpdateChat } from "@/features/chat/hook/useUpdateMessage";
+import { useEffect } from "react";
 
 
 
 export const WelcomeMsg =()=>{
     const {personaInfo} = useUpdateChat()
-    const i = JSON.parse(personaInfo)
+    let i = {name:""}
+    if(personaInfo){
+        i = JSON.parse(personaInfo)
+    }
+    console.log(personaInfo)
+
 
 
     return(
         <div className="space-y-4 mb-4" >
             <h1 className="text-2xl lg:text-4xl text-white font-medium" >
-                Welcome Back, {i.name}
+                Welcome Back, {personaInfo && i.name}
             </h1>
             <QuestionChat />
             <p className="text-sm text-[#dfecff] lg:text-base" >
