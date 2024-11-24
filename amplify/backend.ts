e1bde2e7fa23a2b2a0b5fc59a646155e732db857
Stam2000 <sopemmanuel@gmail.com>
2024-11-24 20:58:19 +0100

    update my function amplify
    update my function amplify

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,10 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { myFirstFunction } from './my-first-function/ressource';
+
 
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
-  myFirstFunction,
+
 });
