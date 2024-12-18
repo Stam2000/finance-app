6d96dfdb972cad2b42472dad5bd4100357378802
Stam2000 <sopemmanuel@gmail.com>
2024-12-18 17:55:57 +0100

    Update create-ai-functions.ts

--- lib/AI/create-ai-functions.ts
+++ lib/AI/create-ai-functions.ts
@@ -18,10 +18,6 @@ type TransactionRequest = (TransactionCreate & {
   detailsTransactions?: DetailsTransactionCreate;
 })[];
 
-export const openai = new OpenAI({
-  apiKey:
-    "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",
-});
 
 const createOneTransactionDef = async (input: string) => {
   let json: TransactionCreate = await JSON.parse(input);
